9afe59a674e604879c9cf9e2fc472b070e566718
Vansh Kalra <vansh@erino.io>
2025-11-21 18:35:13 +0530

    Basic counter program using PDAs

--- programs/anchor-basics/src/lib.rs
+++ programs/anchor-basics/src/lib.rs
@@ -7,10 +7,47 @@ pub mod anchor_basics {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        return Ok(());
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        return Ok(());
     }
 }
 
+// Accounts struct for initialize
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8,
+        seeds = [b"counter", user.key().as_ref()],
+        bump
+    )]
+    pub counter: Account<'info, GmCounter>,
+
+    pub system_program: Program<'info, System>
+}
+
+// Accounts struct for increment
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, seeds = [b"counter", user.key().as_ref()], bump)]
+    pub counter: Account<'info, GmCounter>,
+
+    pub user: Signer<'info>
+}
+
+// PDA Account storing the count
+#[account]
+pub struct GmCounter {
+    pub count: u64
+}
